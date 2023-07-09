c0b26f1ba798de97182ac840cd54e1bee27049c6
luis patino <lcp.patino@gmail.com>
2023-07-09 15:03:35 -0700

    fetch meal category mean data

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 
+const searchTermsInput = document.body.querySelector('#search-terms');
+
+const getMealCategories = async () => {
+    const mealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
+
+    try {
+    const response = await fetch(mealCategoriesApiURL)
+    const data = response.json()
+    console.log(`data: `, data);
+    }   catch (error) {
+    console.log(error)
+    alert('Something went wrong, try again later')
+    }
+}
+
 const handleFormInputFocus = async () => {
   console.log('focus ocurred');
 
